c1bf4a838a26cb57092726ee8963dfb815d913fd
Karlgelbjerg <kgelbjerghan.ieu2022@student.ie.edu>
2024-02-20 10:22:59 +0100

    Session 9 and 10

--- read from file.py
+++ read from file.py
@@ -1,2 +1,10 @@
 file_name = "x-file.txt"
-fd = open(file_name) # r is implicit
+fd = open(file_name)  # r is implicit
+print("here are the contents of the file:")
+print(fd.read())
+fd.close()
+# another way to read file
+for line in fd:
+    print(line)
+
+fd.close()
