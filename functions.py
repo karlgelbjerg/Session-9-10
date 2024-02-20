88327c3aae70fe46db85cf6add19be9639f58ccb
Karlgelbjerg <kgelbjerghan.ieu2022@student.ie.edu>
2024-02-20 11:37:07 +0100

    Session 9 and 10

--- functions.py
+++ functions.py
@@ -38,9 +38,9 @@ def factorial(n):
     :param n: int number
     :return: the value of n!
     """
-if n== 0:
+if n == 0:
         return 1
-    return n* factorial(n - 1)
+    return n * factorial(n - 1)
 
 
 print(factorial(5)) #should be 120
