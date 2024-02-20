82dd70effd41cb946226f5ca3c3c319143a09f53
Karlgelbjerg <kgelbjerghan.ieu2022@student.ie.edu>
2024-02-20 10:29:19 +0100

    Session 9 and 10

--- read from file.py
+++ read from file.py
@@ -3,8 +3,15 @@ fd = open(file_name)  # r is implicit
 print("here are the contents of the file:")
 print(fd.read())
 fd.close()
+
 # another way to read file
 for line in fd:
-    print(line)
+    # print(line, end="")
+    # print(line.strip())
+    print(line.replace("\n", ""))
+    words = line.split()
+    for word in words:
+        if len(word) == 3:\
+        print(word)
 
 fd.close()
